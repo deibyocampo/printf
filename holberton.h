b37b191ae75ddfcb95c2489b9a8dad596544449b
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-27 14:58:43 -0500

    Adding a update to the headers file with the first prototypes of the functions that this project needs.

--- holberton.h
+++ holberton.h
@@ -1,7 +1,11 @@
 #ifndef _printf_
 #define _printf_
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
-void _puts(char *str);
+void _pts(char *str);
+char *_itoa(int n, char *s, int b);
 #endif
 
