a947183f1adac6663e841fa4478818b953ef1615
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-28 19:18:42 -0500

    A first commit from the _printf.c function with the changes that work with the initial requirements.

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * _printf - Function to print anything elements given
+ * as a parameters.
+ * @format: The text with the format (c, i, f, s)
+ *
+ * Author: José Calderón
+ * Description: A function to print various elements from
+ * these formats: char (c), integer (i), float (f) and char*(s)
+ * Return: void
+ */
+int _printf(const char *format, ...)
+{
+	va_list vl;
+	unsigned int i = 0, j = 0, k = 0;
+	char t;
+	char *s;
+
+	va_start(vl, format);
+	while (format[i] != '\0')
+	{
+		t = format[i];
+		if (t == '%' && format[i + 1] == '%')
+		{
+			i++;
+		}
+		if (t == '%' && format[i + 1] == 'c')
+		{
+			t = (char)va_arg(vl, int);
+			i++;
+		}
+		if (t == '%' && format[i + 1] == 's')
+		{
+			s = va_arg(vl, char*);
+			j = 0;
+			while (s[j] != '\0')
+			{
+				_putchar(s[j]);
+				k++;
+				j++;
+			}
+			i = i + 2;
+			continue;
+		}
+		_putchar(t);
+		i++;
+		k++;
+	}
+	va_end(vl);
+	return (k);
+}
