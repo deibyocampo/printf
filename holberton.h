7762e6f3d4630215f312d72203511e9034fca688
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-31 20:14:45 -0500

    [FEATURE] Adding functions _pchar _pnumber _pstring and structure called ops to refatorizing the code in the _printf function.

--- holberton.h
+++ holberton.h
@@ -7,5 +7,20 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _cdigits(int n);
 int _itoa(int n);
-void printnumber(unsigned int *k, va_list vl, unsigned int *i);
+int _pchar(va_list vl);
+int _pstring(va_list vl);
+int _pnumber(va_list vl);
+/**
+ * struct ops - An structure to the _printf options
+ * @o: The option to access
+ * @f: The function asigned to the specific option.
+ *
+ * Description: This is an structure to manage
+ * the diferent options of the _printf function.
+ */
+typedef struct ops
+{
+	char o;
+	int (*f)(va_list lv);
+} op_list;
 #endif
